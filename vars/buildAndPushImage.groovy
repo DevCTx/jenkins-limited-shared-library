fbed4bb1c9b05530b0e515875151f06a40f4b268
DevCTx <contact.devct@gmail.com>
2026-03-31 19:20:30 +0200

    update with image and version in env vars

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -2,18 +2,18 @@
 
 def call(String imageName, String version) {
     echo "docker build and push image ..."
-    def img_ver = "${imageName}:${version}"
-    withCredentials( [
-        usernamePassword( credentialsId: 'dockerhub-credentials',
-                        usernameVariable: 'DOCKER_USERNAME',
-				        passwordVariable: 'DOCKER_PASSWORD' )
-    ]) {
-        // Diagnoses 90% of jenkins+docker errors
-        echo "check docker env"
-        sh "whoami && id && docker --version && docker ps"
 
-        // --rm automatically removes intermediate containers
-        withEnv(["IMG_VER=${img_ver}"]) {
+    withEnv(["IMG_VER=${imageName}:${version}"]) {
+        withCredentials( [
+            usernamePassword( credentialsId: 'dockerhub-credentials',
+                            usernameVariable: 'DOCKER_USERNAME',
+				            passwordVariable: 'DOCKER_PASSWORD' )
+        ]) {
+            // Diagnoses 90% of jenkins+docker errors
+            echo "check docker env"
+            sh "whoami && id && docker --version && docker ps"
+
+            // --rm automatically removes intermediate containers
             sh '''
                 docker build --rm -t "$IMG_VER" .
                 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
