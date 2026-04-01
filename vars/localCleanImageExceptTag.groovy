85d1c0366bcaa79a4a1ebe51d649863377a8f798
DevCTx <contact.devct@gmail.com>
2026-04-01 15:30:55 +0200

    update cleaning functions

--- vars/localCleanImageExceptTag.groovy
+++ vars/localCleanImageExceptTag.groovy
@@ -4,6 +4,7 @@ def call(String imageName, String imageTag) {
     echo "Cleaning all ${imageName} docker images except tag ${imageTag} on local..."
 
     withEnv(["IMG_NAME=${imageName}", "IMG_TAG=${imageTag}"]) {
+
         // clean the images with no tag or <none>
         sh 'docker image prune -f'        
         // // List all tags and ids of the given image, except for the specified tag, and remove all others from those listed ids.
