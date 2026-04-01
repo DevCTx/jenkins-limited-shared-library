799f61f966acedcb9c2dd03a8c9e15469a332398
DevCTx <contact.devct@gmail.com>
2026-04-01 12:23:37 +0200

    add ec2Deploy

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(String imageName, String version) {
+def call(String imageName, String imageTag) {
     echo "docker build and push image ..."
 
-    withEnv(["IMG_VER=${imageName}:${version}"]) {
+    withEnv(["IMG_TAG=${imageName}:${imageTag}"]) {
         withCredentials( [
             usernamePassword( credentialsId: 'dockerhub-credentials',
                             usernameVariable: 'DOCKER_USERNAME',
@@ -15,9 +15,9 @@ def call(String imageName, String version) {
 
             // --rm automatically removes intermediate containers
             sh '''
-                docker build --rm -t "$IMG_VER" .
+                docker build --rm -t "$IMG_TAG" .
                 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-                docker push "$IMG_VER"
+                docker push "$IMG_TAG"
                 docker logout
             '''
         }
---
+++ vars/ec2Deploy.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+
+def call(Srting containerName, String hostPort, String containerPort, String imageName, String imageTag) {
+    echo "Deploying the application..."
+
+    def dockerCmd = """
+        docker pull ${imageName}:${imageTag} &&
+        docker stop ${containerName} || true &&
+        docker rm ${containerName} || true &&
+        docker run -d --name ${containerName} -p ${hostPort}:${containerPort} ${imageName}:${imageTag}
+    """
+
+    withEnv(["DOCKER_CMD=${dockerCmd}"]) {
+        sshagent(['ec2-server-key']) {
+            withCredentials([
+                string(credentialsId: 'EC2_USER', variable: 'EC2_USER'),
+                string(credentialsId: 'EC2_HOST', variable: 'EC2_HOST')
+            ]) {
+                sh 'ssh -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST "$DOCKER_CMD"'
+            }
+        }
+    }
+}
