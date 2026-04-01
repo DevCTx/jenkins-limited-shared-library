e72d468bec9ef099fa4f86b1bf3f11ecb71e774f
DevCTx <contact.devct@gmail.com>
2026-04-01 14:48:39 +0200

    add cleaning functions

---
+++ vars/ec2CleanImageExceptTag.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+def call(String imageName, String imageTag) {
+    echo "Cleaning all ${imageName} docker images except tag ${imageTag} on EC2..."
+
+    def dockerCmd = """
+        docker image prune -f
+        docker images "${imageName}" --format "{{.Tag}} {{.ID}}" | grep -v "${imageTag}" | awk '{print $2}' | xargs -r docker rmi -f
+    """
+
+    withEnv(["DOCKER_CMD=${dockerCmd}"]) {
+        sshagent(['ec2-server-key']) {
+            withCredentials([
+                string(credentialsId: 'EC2_USER', variable: 'EC2_USER'),
+                string(credentialsId: 'EC2_HOST', variable: 'EC2_HOST')
+            ]) {
+                sh 'ssh -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST "$DOCKER_CMD"'
+            }
+        }
+    }
+}
---
+++ vars/localCleanImageExceptTag.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+def call(String imageName, String imageTag) {
+    echo "Cleaning all ${imageName} docker images except tag ${imageTag} on local..."
+
+    withEnv(["IMG_NAME=${imageName}", "IMG_TAG=${imageTag}"]) {
+        // clean the images with no tag or <none>
+        sh 'docker image prune -f'        
+        // // List all tags and ids of the given image, except for the specified tag, and remove all others from those listed ids.
+        sh 'docker images "$IMG_NAME" --format "{{.Tag}} {{.ID}}" | grep -v "$IMG_TAG" | awk '{print $2}' | xargs -r docker rmi -f '
+    }
+}
+
+
+
