fc77757db9f8871fe310d4a5b389a9fbf18873f0
DevCTx <contact.devct@gmail.com>
2026-04-03 17:13:38 +0200

    export IMAGE_TAG for docker compose

--- vars/ec2DockerComposeDeploy.groovy
+++ vars/ec2DockerComposeDeploy.groovy
@@ -6,17 +6,19 @@
 def call(String containerName, String hostPort, String containerPort, String imageName, String imageTag) {
     echo "Deploying the application via Docker Compose on EC2 ... "
 
-    def imageNameTag = "${imageName}:${imageTag}"
-    def dockerCmd = "docker compose -f docker-compose.yaml up"
+    def dockerCmd = """
+        export IMAGE_TAG=${imageName}:${imageTag}
+        docker-compose -f docker-compose.yaml up
+    """"
 
-    withEnv(["IMAGE_TAG=${imageNameTag}", "DOCKER_CMD=${dockerCmd}"]) {
+    withEnv(["DOCKER_CMD=${dockerCmd}"]) {
         sshagent(['ec2-server-key']) {
             withCredentials([
                 string(credentialsId: 'EC2_USER', variable: 'EC2_USER'),
                 string(credentialsId: 'EC2_HOST', variable: 'EC2_HOST')
             ]) {
                 sh 'scp docker-compose.yaml $EC2_USER@$EC2_HOST:/home/$EC2_USER/ '
-                sh 'ssh -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST "IMAGE_TAG=$IMAGE_TAG $DOCKER_CMD"'
+                sh 'ssh -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST "$DOCKER_CMD"'
             }
         }
     }
