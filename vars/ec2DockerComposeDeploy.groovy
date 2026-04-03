33252ad99074cb055e4f7bcd67820aa8d33009c2
DevCTx <contact.devct@gmail.com>
2026-04-03 16:43:02 +0200

    using docker compose on ec2

---
+++ vars/ec2DockerComposeDeploy.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+//
+// ec2DockerComposeDeploy.groovy
+//
+
+def call(String containerName, String hostPort, String containerPort, String imageName, String imageTag) {
+    echo "Deploying the application via Docker Compose on EC2 ... "
+
+    def dockerCmd = "docker-compose -f docker-compose.yaml up --detach"
+
+    withEnv(["DOCKER_CMD=${dockerCmd}"]) {
+        sshagent(['ec2-server-key']) {
+            withCredentials([
+                string(credentialsId: 'EC2_USER', variable: 'EC2_USER'),
+                string(credentialsId: 'EC2_HOST', variable: 'EC2_HOST')
+            ]) {
+                sh 'scp docker-compose.yaml $EC2_USER@$EC2_HOST:/home/$EC2_USER/ '
+                sh 'ssh -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST "$DOCKER_CMD"'
+            }
+        }
+    }
+}
