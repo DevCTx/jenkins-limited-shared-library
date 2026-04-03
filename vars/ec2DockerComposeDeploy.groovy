3694751e973f8f3e6ab8ffa46eca30328720a58c
DevCTx <contact.devct@gmail.com>
2026-04-03 16:56:54 +0200

    imageTag in args for docker compose

--- vars/ec2DockerComposeDeploy.groovy
+++ vars/ec2DockerComposeDeploy.groovy
@@ -6,16 +6,17 @@
 def call(String containerName, String hostPort, String containerPort, String imageName, String imageTag) {
     echo "Deploying the application via Docker Compose on EC2 ... "
 
-    def dockerCmd = "docker-compose -f docker-compose.yaml up --detach"
+    def imageTag = "${imageName}:${imageTag}"
+    def dockerCmd = "docker compose -f docker-compose.yaml up"
 
-    withEnv(["DOCKER_CMD=${dockerCmd}"]) {
+    withEnv(["IMAGE_TAG=${imageTag}", "DOCKER_CMD=${dockerCmd}"]) {
         sshagent(['ec2-server-key']) {
             withCredentials([
                 string(credentialsId: 'EC2_USER', variable: 'EC2_USER'),
                 string(credentialsId: 'EC2_HOST', variable: 'EC2_HOST')
             ]) {
                 sh 'scp docker-compose.yaml $EC2_USER@$EC2_HOST:/home/$EC2_USER/ '
-                sh 'ssh -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST "$DOCKER_CMD"'
+                sh 'ssh -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST "IMAGE_TAG=$IMAGE_TAG $DOCKER_CMD"'
             }
         }
     }
