b67098be31b5c834805ddee666704f0bf77c6e76
DevCTx <contact.devct@gmail.com>
2026-04-19 19:37:58 +0200

    debugging BuildJar

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -5,9 +5,5 @@
 
 def call() {
     echo "Building JAR file ..."
-    sh "pwd"
-    sh "ls -R"
-    sh "find . -name pom.xml"    
-
-    sh "mvn --no-transfer-progress -f pom.xml clean package"
+    sh "mvn --no-transfer-progress clean package"
 }
