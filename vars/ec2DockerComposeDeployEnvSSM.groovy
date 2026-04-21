d659a70b44a6aed0f0dd284d1cd9956d39fca381
DevCTx <contact.devct@gmail.com>
2026-04-21 23:41:16 +0200

    Update ec2DockerComposeDeployEnvSSM

--- vars/ec2DockerComposeDeployEnvSSM.groovy
+++ vars/ec2DockerComposeDeployEnvSSM.groovy
@@ -6,35 +6,45 @@ def call() {
 
     echo "Deploying application via AWS SSM..."
 
-    withCredentials([string(credentialsId: 'PROD_EC2_ID', variable: 'PROD_EC2_ID')]) {
-        withEnv(["EC2_ID=${PROD_EC2_ID}", "BUCKET=bucket/app/${env.BUILD_NUMBER}"]) {
-        
+    // Encode the local files in base64 to facilitate the transfert 
+    // but only with shell or it will be blocked with groovy policies
+    def dot_env = sh(
+        script: "base64 -w 0 .env",
+        returnStdout: true
+    ).trim()
+
+    def docker_compose = sh(
+        script: "base64 -w 0 docker-compose.yaml",
+        returnStdout: true
+    ).trim()
+
+    withEnv([
+        "DOT_ENV=${dot_env}", 
+        "DOCKER_COMPOSE=${docker_compose}"
+    ]) {        
+        withCredentials([
+            string(credentialsId: 'PROD_EC2_ID', variable: 'PROD_EC2_ID')
+        ]) {
             sh '''
-            set -e
-
-            aws s3 cp docker-compose.yaml s3://${BUCKET}/
-            aws s3 cp .env s3://${BUCKET}/
-
             aws ssm send-command \
               --document-name "AWS-RunShellScript" \
-              --instance-ids ${EC2_ID} \
+              --instance-ids $PROD_EC2_ID \
               --region eu-west-3 \
               --comment "Docker Compose with Env deployment" \
               --parameters commands='[
-
-                    "export BUCKET='"$BUCKET"' ",
-
                     "sudo mkdir -p /opt/app",
                     "sudo chown -R ec2-user:docker /opt/app || true",
 
-                    "aws s3 cp s3://$BUCKET/.env /opt/app/.env",
-                    "aws s3 cp s3://$BUCKET/docker-compose.yaml /opt/app/docker-compose.yaml",
+                    "echo '"$DOT_ENV"' | base64 -d > /opt/app/.env",
+                    "echo '"$DOCKER_COMPOSE"' | base64 -d > /opt/app/docker-compose.yaml",
 
-                    "cd /opt/app",
-                    "docker compose down --remove-orphans || true",
-                    "docker compose up -d --quiet-pull" 
+                    "cd /opt/app && docker compose down --remove-orphans || true",
+                    "cd /opt/app && docker compose up -d --quiet-pull" 
                 ]'
-            '''
+            ''' 
+            // cd /opt/app alone is useless in SSM
+            // Each command in the SSM table runs in an independent shell 
+            // so the cd does not persist for the next command.
         }
     }
 }
