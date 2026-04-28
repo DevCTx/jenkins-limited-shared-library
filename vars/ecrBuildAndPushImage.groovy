b2e2eaea5d92ba1d96b69055cddaf003083fe2f8
DevCTx <contact.devct@gmail.com>
2026-04-28 16:19:55 +0200

    change methods

--- vars/ecrBuildAndPushImage.groovy
+++ vars/ecrBuildAndPushImage.groovy
@@ -6,26 +6,26 @@ def call() {
     echo "docker build and push image to ECR  ..."
 
     withCredentials( [
-        string(credentialsId: 'ECR_REGITRY', variable: 'ECR_REGITRY')
+        string(credentialsId: 'ECR_REGISTRY', variable: 'ECR_REGISTRY')
     ]) {
-        echo "Build of ${ECR_REGITRY}/${APP_IMAGE_NAME}:${APP_IMAGE_TAG}"
+        echo "Build of ${ECR_REGISTRY}/${APP_IMAGE_NAME}:${APP_IMAGE_TAG}"
         sh "docker build --rm -t ${ECR_REGITRY}/${APP_IMAGE_NAME}:${APP_IMAGE_TAG} ."
 
-        echo "Push of ${ECR_REGITRY}/${APP_IMAGE_NAME}:${APP_IMAGE_TAG}"
+        echo "Push of ${ECR_REGISTRY}/${APP_IMAGE_NAME}:${APP_IMAGE_TAG}"
         sh """
             # Verify IAM role
             aws sts get-caller-identity
 
             # Create ECR repo if not exists
-            aws ecr describe-repositories --repository-names ${ECR_REGITRY}/${APP_IMAGE_NAME} --region eu-west-3 \
-                || aws ecr create-repository --repository-name ${ECR_REGITRY}/${APP_IMAGE_NAME} --region eu-west-3
+            aws ecr describe-repositories --repository-names ${ECR_REGISTRY}/${APP_IMAGE_NAME} --region eu-west-3 \
+                || aws ecr create-repository --repository-name ${ECR_REGISTRY}/${APP_IMAGE_NAME} --region eu-west-3
 
             # Login ECR via IAM role
             aws ecr get-login-password --region eu-west-3 \
-                | docker login --username AWS --password-stdin ${ECR_REGITRY}
+                | docker login --username AWS --password-stdin ${ECR_REGISTRY}
 
-            docker push ${ECR_REGITRY}/${APP_IMAGE_NAME}:${APP_IMAGE_TAG} --quiet
-            docker logout $ECR_REPOSITORY/${APP_IMAGE_NAME}
+            docker push ${ECR_REGISTRY}/${APP_IMAGE_NAME}:${APP_IMAGE_TAG} --quiet
+            docker logout $ECR_REGISTRY/${APP_IMAGE_NAME}
         """
     }
 }
