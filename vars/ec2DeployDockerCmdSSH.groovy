996eb30e11c3ba1106e09e42ec639d1244f90ecf
DevCTx <contact.devct@gmail.com>
2026-04-29 12:49:01 +0200

    Update pipelines

--- vars/ec2DeployDockerCmdSSH.groovy
+++ vars/ec2DeployDockerCmdSSH.groovy
@@ -14,17 +14,17 @@ def call() {
                 ssh -o StrictHostKeyChecking=no ec2-user@$MY_INSTANCE_EC2_IP '
                     set -e
 
-                    echo "Deploying $DOCKER_USERNAME/$APP_IMAGE_NAME:$APP_IMAGE_TAG"
+                    echo "Deploying ${DOCKER_USERNAME}/${APP_IMAGE_NAME}:{$APP_IMAGE_TAG}"
 
-                    docker pull "$DOCKER_USERNAME/$APP_IMAGE_NAME:$APP_IMAGE_TAG"
+                    docker pull "${DOCKER_USERNAME}/${APP_IMAGE_NAME}:{$APP_IMAGE_TAG}"
 
-                    docker stop "$APP_CONTAINER_NAME" || true
-                    docker rm "$APP_CONTAINER_NAME" || true
+                    docker stop "${APP_CONTAINER_NAME}" || true
+                    docker rm "${APP_CONTAINER_NAME}" || true
 
                     docker run -d \
-                        --name "$APP_CONTAINER_NAME" \
-                        -p "$APP_HOST_PORT:$APP_CONTAINER_PORT" \
-                        "$DOCKER_USERNAME/$APP_IMAGE_NAME:$APP_IMAGE_TAG"
+                        --name "${APP_CONTAINER_NAME}" \
+                        -p "${APP_HOST_PORT}:{$APP_CONTAINER_PORT}" \
+                        "${DOCKER_USERNAME}/${APP_IMAGE_NAME}:{$APP_IMAGE_TAG}"
                 '
             '''
         }
